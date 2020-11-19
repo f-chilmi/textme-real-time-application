ffaf94f52c2cb5eb536ef8f4a2d23e33fdf92919
f-chilmi <fchilmi5@gmail.com>
2020-11-19 14:49:43 +0700

    main screen

---
+++ src/screens/Chat.js
@@ -0,0 +1,163 @@
+import React, {Component} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Image,
+} from 'react-native';
+import {Icon, Thumbnail} from 'native-base';
+import {SearchBar} from 'react-native-elements';
+
+export default class Chat extends Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {
+    this.setState({search});
+  };
+  render() {
+    return (
+      <View style={style.parent}>
+        <View style={style.rowDir}>
+          <TouchableOpacity>
+            <Text style={style.linked}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.iconRight}>
+            <Icon
+              name="create"
+              type="MaterialIcons"
+              size={20}
+              color="#0000cd"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.header}>
+          <Text style={style.title}>Chat</Text>
+        </View>
+        <ScrollView>
+          <SearchBar
+            platform="ios"
+            searchIcon={
+              <Icon
+                name="search"
+                type="MaterialIcons"
+                size={16}
+                color="#808080"
+              />
+            }
+            inputStyle={style.inputSearch}
+            containerStyle={style.containerStyle}
+            inputContainerStyle={style.inputcontainerStyle}
+            cancelButtonProps={style.cancelButton}
+            placeholder="Cari"
+            onChangeText={this.updateSearch}
+            value={this.state.search}
+          />
+
+          <View style={style.rowDir2}>
+            <TouchableOpacity>
+              <Text style={style.linked}>Daftar Siaran</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.iconRight}>
+              <Text style={style.linked}>Grup Baru</Text>
+            </TouchableOpacity>
+          </View>
+
+          {Array(30).fill(
+            <View style={style.rowChat}>
+              <View style={style.thumbnailWrap}>
+                <Thumbnail source={require('../assets/5fa3e598894a4.jpg')} />
+              </View>
+              <View style={style.centerTextContent}>
+                <Text style={style.sender}>Nama pengirim</Text>
+                <Text style={style.content}>
+                  Isi chat sangat panjang Isi chat sangat panjang Isi chat
+                  sangat panjang Isi chat{' '}
+                </Text>
+              </View>
+              <View>
+                <Text style={style.content}>28/10/20</Text>
+              </View>
+            </View>,
+          )}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  parent: {
+    flex: 1,
+    padding: '3%',
+    backgroundColor: 'white',
+  },
+  linked: {
+    color: '#1e90ff',
+  },
+  rowDir: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  iconRight: {
+    marginLeft: 'auto',
+  },
+  header: {
+    height: '10%',
+    // backgroundColor: 'yellow'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 27,
+  },
+  inputSearch: {
+    fontSize: 12,
+  },
+  containerStyle: {
+    padding: 0,
+  },
+  inputcontainerStyle: {
+    height: 25,
+  },
+  cancelButton: {
+    color: 'grey',
+  },
+  rowDir2: {
+    flexDirection: 'row',
+    width: '100%',
+    borderBottomWidth: 1,
+    borderColor: '#dcdcdc',
+    paddingBottom: 10,
+    paddingTop: 10,
+    // backgroundColor: 'yellow',
+  },
+  rowChat: {
+    paddingVertical: '2%',
+    flexDirection: 'row',
+    height: 70,
+    // backgroundColor: 'yellow',
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    borderColor: '#dcdcdc',
+  },
+  thumbnailWrap: {
+    marginRight: 10,
+  },
+  centerTextContent: {
+    width: 220,
+    // backgroundColor: 'yellow',
+  },
+  sender: {
+    fontWeight: '700',
+    fontSize: 11,
+  },
+  content: {
+    fontSize: 9,
+    color: 'grey',
+  },
+});
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {connect} from 'react-redux';
 import {Icon} from 'native-base';
 
@@ -8,7 +9,19 @@ const Stack = createStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 // import screen
-import Home from './Home';
+import Chat from './Chat';
+
+const MainStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen 
+        options={{headerShown: false}}
+        name="Chat"
+        component={Chat}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default class Main extends Component {
   render() {
@@ -18,11 +31,11 @@ export default class Main extends Component {
           <BottomTabs.Screen
             options={{
               tabBarIcon: ({size, color, focused}) => (
-                <Icon name="home" size={20} color={color} />
+                <Icon name="chat" size={20} color={color} type="MaterialIcons" />
               ),
             }}
-            name="Home"
-            component={Home}
+            name="Chat"
+            component={MainStack}
           />
         </BottomTabs.Navigator>
       </NavigationContainer>
