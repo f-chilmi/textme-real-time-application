75d4c76395f1e4174a497e6c9f35fa4a5b3de758
f-chilmi <fchilmi5@gmail.com>
2020-11-19 15:03:37 +0700

    fixing main screen

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,95 +1,93 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
   View,
   TouchableOpacity,
   StyleSheet,
   Text,
   ScrollView,
-  SafeAreaView,
-  Image,
+  FlatList,
 } from 'react-native';
 import {Icon, Thumbnail} from 'native-base';
 import {SearchBar} from 'react-native-elements';
 
-export default class Chat extends Component {
-  state = {
-    search: '',
-  };
+const Chat = () => {
+  const [search, setSearch] = React.useState('');
+  const renderItem = ({item}) => (
+    <TouchableOpacity style={style.rowChat}>
+      <View style={style.thumbnailWrap}>
+        <Thumbnail source={require('../assets/5fa3e598894a4.jpg')} />
+      </View>
+      <View style={style.centerTextContent}>
+        <Text style={style.sender}>Nama pengirim</Text>
+        <Text style={style.content}>
+          Isi chat sangat panjang Isi chat sangat panjang Isi chat sangat
+          panjang Isi chat{' '}
+        </Text>
+      </View>
+      <View>
+        <Text style={style.content}>28/10/20</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
-  updateSearch = (search) => {
-    this.setState({search});
-  };
-  render() {
-    return (
-      <View style={style.parent}>
-        <View style={style.rowDir}>
+  return (
+    <View style={style.parent}>
+      <View style={style.rowDir}>
+        <TouchableOpacity>
+          <Text style={style.linked}>Edit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={style.iconRight}>
+          <Icon name="create" type="MaterialIcons" size={20} color="#0000cd" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={style.header}>
+        <Text style={style.title}>Chat</Text>
+      </View>
+      <ScrollView>
+        <SearchBar
+          platform="ios"
+          searchIcon={
+            <Icon
+              name="search"
+              type="MaterialIcons"
+              size={16}
+              color="#808080"
+            />
+          }
+          inputStyle={style.inputSearch}
+          containerStyle={style.containerStyle}
+          inputContainerStyle={style.inputcontainerStyle}
+          cancelButtonProps={style.cancelButton}
+          placeholder="Cari"
+          onChangeText={() => setSearch(text)}
+          value={search}
+        />
+
+        <View style={style.rowDir2}>
           <TouchableOpacity>
-            <Text style={style.linked}>Edit</Text>
+            <Text style={style.linked}>Daftar Siaran</Text>
           </TouchableOpacity>
           <TouchableOpacity style={style.iconRight}>
-            <Icon
-              name="create"
-              type="MaterialIcons"
-              size={20}
-              color="#0000cd"
-            />
+            <Text style={style.linked}>Grup Baru</Text>
           </TouchableOpacity>
         </View>
+        <FlatList
+          data={Array(30)}
+          renderItem={renderItem}
+          keyExtractor={(item) => item}
+          // numColumns={2}
+          // onEndReached={nextPage}
+          // onEndReachedThreshold={0.5}
+          // refreshing={loading}
+          // onRefresh={() => dispatch(homeAction.refreshCatalog())}
+        />
+      </ScrollView>
+    </View>
+  );
+};
 
-        <View style={style.header}>
-          <Text style={style.title}>Chat</Text>
-        </View>
-        <ScrollView>
-          <SearchBar
-            platform="ios"
-            searchIcon={
-              <Icon
-                name="search"
-                type="MaterialIcons"
-                size={16}
-                color="#808080"
-              />
-            }
-            inputStyle={style.inputSearch}
-            containerStyle={style.containerStyle}
-            inputContainerStyle={style.inputcontainerStyle}
-            cancelButtonProps={style.cancelButton}
-            placeholder="Cari"
-            onChangeText={this.updateSearch}
-            value={this.state.search}
-          />
-
-          <View style={style.rowDir2}>
-            <TouchableOpacity>
-              <Text style={style.linked}>Daftar Siaran</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={style.iconRight}>
-              <Text style={style.linked}>Grup Baru</Text>
-            </TouchableOpacity>
-          </View>
-
-          {Array(30).fill(
-            <View style={style.rowChat}>
-              <View style={style.thumbnailWrap}>
-                <Thumbnail source={require('../assets/5fa3e598894a4.jpg')} />
-              </View>
-              <View style={style.centerTextContent}>
-                <Text style={style.sender}>Nama pengirim</Text>
-                <Text style={style.content}>
-                  Isi chat sangat panjang Isi chat sangat panjang Isi chat
-                  sangat panjang Isi chat{' '}
-                </Text>
-              </View>
-              <View>
-                <Text style={style.content}>28/10/20</Text>
-              </View>
-            </View>,
-          )}
-        </ScrollView>
-      </View>
-    );
-  }
-}
+export default Chat;
 
 const style = StyleSheet.create({
   parent: {
